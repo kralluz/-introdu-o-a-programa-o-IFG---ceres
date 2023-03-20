54169164ea782beaf856170bbd9a8113c229a704
Carlos_klz <97392902+kralluz@users.noreply.github.com>
2023-03-20 14:36:51 -0300

    Update 001.cpp

--- exercicio 01/001.cpp
+++ exercicio 01/001.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main(int argc, char** argv){
-std::cout << "digite a operação matemática ";
+int main()
+{
+    float num1, num2;
+
+    //Recebendo os dados
+    cout << "Primeiro numero: ";
+    cin >> num1;
+
+    cout << "Segundo numero: ";
+    cin >> num2;
+
+    //Exibindo as operações
+    cout << "Soma         : " << num1 + num2 << endl;
+    cout << "Subtracao    : " << num1 - num2 << endl;
+    cout << "Multiplicacao: " << num1 * num2 << endl;
+    cout << "Divisao      : " << num1 / num2 << endl;
+
+    return 0;
 }
