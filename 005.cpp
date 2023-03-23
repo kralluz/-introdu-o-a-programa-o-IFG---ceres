a75faa5a683127dc1b5308afbe9fcff147fcc80c
Carlos_klz <chenrique201520@gmail.com>
2023-03-23 11:56:28 -0300

    a

--- 005.cpp
+++ 005.cpp
@@ -1,33 +1,28 @@
-//Faça um programa que receba o preço de custo de um produto e a
-//margem de lucro sobre o mesmo em porcentagem (%), 
-//calcule e mostre o preço de venda já com o lucro incluído.
-
-
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    string nameproduct; // Declaração da variável name como uma string já que a string armazena vários caracteres
-    float custo, margem, precofinal; // Declaração das variáveis do preço de custo, margem de lucro
+    // Este programa calcula o preço final de um produto com base no seu custo de mercado e na margem de lucro desejada pelo usuário.
 
-    // Recebendo os dados (nome completo do usuário)
-    cout << "Qual o seu nome do produto?\n";
-    getline(cin, nameproduct); // Utiliza a função getline para ler o nome do usuário com espaços em branco
+    string nameproduct; // Armazena o nome do produto
+    float custo, margem, precofinal; // Armazena o custo, a margem de lucro e o preço final do produto
 
-    cout << "Digite o custo de mercado do produto!\n";
-    cin >> custo; // Lê as notas bimestrais do usuário
+    // Seção de entrada de dados - solicita ao usuário o nome e o custo do produto, e a margem de lucro desejada.
+    cout << "Qual o nome do produto? Digite o nome completo do produto.\n";
+    getline(cin, nameproduct); // Lê o nome do produto digitado pelo usuário e armazena na variável 'nameproduct'
     
-    cout << "Digite o a margem de lucro desejada sobre o produto em porcentagem!\n";
-    cin >> margem; // Lê a margem de lucro
+    cout << "Digite o custo de mercado do produto em reais:\n";
+    cin >> custo; // Lê o custo do produto digitado pelo usuário e armazena na variável 'custo'
 
-    // Cálculo de preço final de acordo com a margem de lucro.
+    cout << "Digite a margem de lucro desejada sobre o produto em porcentagem:\n";
+    cin >> margem; // Lê a margem de lucro digitada pelo usuário e armazena na variável 'margem'
+
+    // Cálculo do preço final do produto
     precofinal = custo + (custo/100)*margem;
 
-    // Exibição das médias
-    cout << "Olá, o " << nameproduct << " tem um custo de mercado de R$" << custo << "  e o preço final é de R$"<<precofinal << endl; // Exibe a média anual
-
+    // Exibição do resultado na tela
+    cout << "O produto " << nameproduct << " tem um custo de mercado de R$" << custo << " e o preço final é de R$" << precofinal << endl;
 
     return 0;
 }
