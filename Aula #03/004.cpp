19f3096094d722981237af3d063509f7009b1b44
Carlos_klz <97392902+kralluz@users.noreply.github.com>
2023-03-23 22:27:21 -0300

    a

---
+++ Aula #03/004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float temperatura;
+
+    //Recebendo os dados
+    cout << "Digite a temperatura em Célcius.\n";
+    cin >> temperatura;
+   temperatura = (temperatura*9/5) + 32
+    cout << "a temperatura em graus farenheit é: "<<temperatura;
+
+    return 0;
+}
