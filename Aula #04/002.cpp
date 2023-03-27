cd85a76e7ddc7baf5e8e38f7bbd652fa863bf0d3
Carlos_klz <chenrique201520@gmail.com>
2023-03-27 14:52:51 -0300

    a

---
+++ Aula #04/002.cpp
@@ -0,0 +1,30 @@
+/*  Faça um programa que
+receba o número de horas trabalhadas e 
+o valor do salário mínimo,
+calcule e mostre o salário a receber por um funcionário seguindo estas regras:
+- a hora trabalhada vale um quinto do salário mínimo;
+- o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora
+- o imposto equivale a 3% do salário bruto;
+- o salário a receber equivale ao salário bruto menos o imposto.
+trabalhada; */
+    #include <iostream>
+    using namespace std;
+
+    int main()
+    {
+        float horas_trabalhadas, valor_salario_minimo, salario_bruto;
+
+        cout << "Quantas horas trabalhadas?\n";
+        cin >> horas_trabalhadas;
+        cout << "Qual o valor do Salário minimo?\n";
+        cin >> valor_salario_minimo;
+
+        float valor_hora_trabalhadas = valor_salario_minimo / 5;
+        salario_bruto = horas_trabalhadas*valor_hora_trabalhadas;
+        float imposto = salario_bruto*0.03;
+        float salario_final = salario_bruto-imposto;
+
+        cout <<"O salário a receber por um funcionário é R$"<<salario_final<<endl;
+
+        return 0;
+    }   
