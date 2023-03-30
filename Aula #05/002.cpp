4109e983dff57f246c40539b4dd8c60f94bfab0c
Carlos_klz <chenrique201520@gmail.com>
2023-03-30 17:28:45 -0300

    Create 002.cpp

---
+++ Aula #05/002.cpp
@@ -0,0 +1,5 @@
+/* Para resolver esse problema, é necessário utilizar o Teorema de Pitágoras. Sabemos que a distância que a escada deve estar da parede é a hipotenusa de um triângulo retângulo, cujos catetos são a altura em que o quadro deve ser pregado e o tamanho da escada. Assim, podemos calcular a distância com a seguinte fórmula:
+
+d = sqrt(h^2 + e^2)
+
+onde d é a distância que a escada deve estar da parede, h é a altura em que o quadro deve ser pregado e e é o tamanho da escada. */
