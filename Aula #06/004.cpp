3db429034a4605d7c47901138b640a0a41fe30b2
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-04-17 22:14:30 -0300

    Create 004.cpp

---
+++ Aula #06/004.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int media_1, media_2;
+    cout << "digite a primeira média bimestral" << endl;
+    cin >> media_1;
+    
+        cout << "digite a segunda média bimestral" << endl;
+        cin >> media_2;
+        
+       float  media_semestral = (media_2 + media_1) / 2;
+        
+    if (media_semestral >= 6) {
+        cout << "Aprovado!";
+    } 
+    else {
+        cout << "Reprovado!";
+    };
+    return 0;
+}
