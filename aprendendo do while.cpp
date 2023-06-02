37ee648548acfab5e0c9d4099d34e1be23676283
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-06-02 20:17:09 -0300

    Update aprendendo do while.cpp

--- aprendendo do while.cpp
+++ aprendendo do while.cpp
@@ -1,56 +1,48 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
+#include<iostream>
+#include<cmath> 
 using namespace std;
+
+
+// Construa um programa em C++ que calcule o valor dos dez primeiros termos da série H, em que:
+// H = (1 / 13) - (1 / 33) + (1 / 53) - (1 / 73) + (1 / 93)
+
 
 int main() {
-    srand(time(0)); // inicializa a semente do gerador de números aleatórios
-    int numero_correto = rand() % 100 + 1; // gera um número aleatório entre 1 e 100
-    int palpite;
 
-    do {
-        cout << "Adivinhe um número entre 1 e 100: ";
-        cin >> palpite;
+float denominador = 13 ;    
+float numerador = 1;
+int pairOrEven = 1;
+float H;
+// primeira operação 
+float PrimeiraOP;
+// operação seguinte
+float SegundaOP;
 
-        if (palpite == numero_correto) {
-            cout << "Parabéns, você acertou!\n";
-        } else if (palpite < numero_correto) {
-            cout << "O número correto é maior.\n";
-        } else {
-            cout << "O número correto é menor.\n";
+
+    for (int contador = 0; contador <= 10; contador++){
+        
+        //ímpar ou par 
+        if(pairOrEven % 2 ) { // SE FOR PAR
+            PrimeiraOP = numerador / denominador; //
+            denominador = denominador + 20;
+            SegundaOP = numerador / denominador;
+            H = PrimeiraOP - SegundaOP;
+            cout <<"na " << contador <<  " vez, H vale:" << H << endl;
+
+
+        }else { // SE FOR ÍMPAR
+            PrimeiraOP = numerador / denominador; //
+            denominador = denominador + 20;
+            SegundaOP = numerador / denominador;
+            H = PrimeiraOP + SegundaOP;
+            cout <<"na " << contador <<  " vez, H vale:" << H << endl;
+
         }
 
-        cout << "Pressione Enter para continuar...";
-        cin.ignore(); // aguarda o usuário pressionar Enter
-        system("clear"); // limpa a tela (no Linux ou macOS) ou system("cls") (no Windows)
-    } while (palpite != numero_correto);
-    
-}
-// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
 
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-    int chute, numeroImaginado, quantidadeDeChute = 0;
-    std::cin >> numeroImaginado;
-    std::cout << "\e[2j";
-    do {
-    std::cout << "tente adivinhar!" << std::endl;
-    std::cin >> chute;
-    quantidadeDeChute = quantidadeDeChute++;
-    if (chute > numeroImaginado){
-        std::cout << "chutou auto" << std::endl;
-    }else if(chute < numeroImaginado){
-        std::cout << "chutou baixo" << std::endl;
+        pairOrEven = pairOrEven + 1;
+        PrimeiraOP = SegundaOP;
     }
-    
-    } while(chute != numeroImaginado);
-    
-    std::cout << "vc precisou de " << quantidadeDeChute << "para acertar" << std::endl;
-    
-    return 0;
+
+
 }
